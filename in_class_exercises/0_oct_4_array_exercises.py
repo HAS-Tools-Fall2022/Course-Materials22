d85389ff69eea29b9978c15afb6480014258a48e
Andrew Bennett <andrbenn@arizona.edu>
2022-10-03 21:24:44 -0700

    Update exercises for tomorrow

--- in_class_exercises/0_oct_4_array_exercises.py
+++ in_class_exercises/0_oct_4_array_exercises.py
@@ -31,6 +31,11 @@ print(y)
 
 #%%
 # 8. How do you get the middle value out of y?
+y = np.array([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+])
 
 #%%
 # 9. How do you get the first row out of y?
@@ -46,6 +51,18 @@ print(z.shape)
 
 #%%
 # 12. How do you get the "center" value from z?
+z = np.array([
+    [[ 0, 1, 2],
+     [ 3, 4, 5],
+     [ 6, 7, 8]],
+    [[ 9,10,11],
+     [12,13,14],
+     [15,16,17]],
+    [[18,19,20],
+     [21,22,23],
+     [24,25,26]]
+])
+
 
 #%%
 # 13. How do you take the total sum of z?
